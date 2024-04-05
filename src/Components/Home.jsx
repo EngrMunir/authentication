052d99fb8fb03513a29d778af5508fdcced4daab
Munir <smsirajulmonir@gmail.com>
2024-04-05 09:21:51 +0600

    create user implemented in context

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
-import { AuthContext } from "../Providers/AuthProvider";
 
 const Home = () => {
-    const authInfo = useContext(AuthContext)
-    console.log(authInfo)
+    
     return (
         <div>
             <h2 className="text-3xl">This is home</h2>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,12 +1,27 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext)
+
+  console.log(authInfo);
+
     const handleRegister = e=>{
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;        
         console.log(email, password, name);
+
+        // create user in firebase
+        createUser(email, password)
+        .then( result =>{
+          console.log(result);
+        })
+        .catch( error =>{
+          console.error(error)
+        })
 
     }
     return (
